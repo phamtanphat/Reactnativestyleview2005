8da0efe3393a58e7d98a462302418f58c41aacd3
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-07-03 19:07:30 +0700

    a

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import View1 from './components/View1';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View1/>
+    );
+  }
+}
