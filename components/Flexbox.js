188562289ae3037c45e67d695dbab24ed3db92ef
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-07-05 18:25:35 +0700

    a

--- components/Flexbox.js
+++ components/Flexbox.js
@@ -5,7 +5,9 @@ export default class Flexbox extends PureComponent {
     render() {
         return (
             <View>
-                <Text> textInComponent </Text>
+                <Text style={{color : "red" , fontSize : 20, backgroundColor : "yellow"}}>View1</Text>
+                <Text style={{color : "red" , fontSize : 20, backgroundColor : "green"}}>View2</Text>
+                <Text style={{color : "red" , fontSize : 20, backgroundColor : "blue"}}>View3</Text>
             </View>
         )
     }
