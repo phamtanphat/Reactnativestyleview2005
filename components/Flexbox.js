e601afa0314c6bac946449e2eec1bb2a64eb41ff
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-07-08 18:05:06 +0700

    a

--- components/Flexbox.js
+++ components/Flexbox.js
@@ -8,11 +8,11 @@ export default class Flexbox extends PureComponent {
                 {/* Group 1 :  1 3 4*/}
                 <View style={{flex : 0.5 , backgroundColor : "red" , flexDirection : "row"}}> 
                         {/* Group 3 :  1 */}
-                    <View style={{flex : 0.2}}>
-                        <Text>1</Text>
+                    <View style={{flex : 0.2 ,justifyContent : "center" , alignItems : "center" , backgroundColor : "green"}}>
+                        <Text style={{fontSize : 30 }}>1</Text>
                     </View>
                         {/* Group 4 :   3 4 */}
-                    <View style={{flex : 0.8}}>
+                    <View style={{flex : 0.8 , backgroundColor : "white"}}>
                         <Text>3</Text>
                         <Text>4</Text>
                     </View>
