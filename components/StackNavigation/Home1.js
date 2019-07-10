0916e1394383d1f06c703b9f1a30357b8a79562c
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-07-10 18:33:19 +0700

    a

--- components/StackNavigation/Home1.js
+++ components/StackNavigation/Home1.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import { Text, View , TouchableOpacity} from 'react-native'
+import { Text, View , TouchableOpacity } from 'react-native'
 
 export default class Home1 extends PureComponent {
     static navigationOptions = {
@@ -16,11 +16,11 @@ export default class Home1 extends PureComponent {
         return (
             <View style={{flex : 1 , justifyContent : "center" , alignItems : "center"}}>
                 <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("Home2")}
                     style={{backgroundColor : "gray" , padding : 10 , borderRadius : 5 }}
                 >
                     <Text style={{color : "red" , fontSize : 20}}> Component1</Text>
                 </TouchableOpacity>
-                
             </View>
         )
     }
