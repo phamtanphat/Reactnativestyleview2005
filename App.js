eb4d47474be738c8b458234f632bc3e58672d17a
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-07-10 19:05:07 +0700

    a

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import { Text, View , StyleSheet} from 'react-native'
 
 import Stack from './components/StackNavigation/stack';
+import Cameragallery from './components/Cameragallery';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{flex : 1}}>
-        <Stack/>
+        <Cameragallery/>
       </View>
     );
   }
---
+++ components/Cameragallery.js
@@ -0,0 +1,119 @@
+import React, { PureComponent } from 'react'
+import { Text, View , SafeAreaView , TouchableOpacity , Image , StyleSheet} from 'react-native'
+// import ImagePicker from 'react-native-image-picker'
+
+
+const options = {
+    title: 'Open camera',
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+}
+export default class Cameragallery extends PureComponent {
+    constructor(props){
+        super(props)
+        this.state = {
+            image : ""
+        }
+    }
+    _takephoto(){
+        
+    }
+    _imagegallery(){
+        
+    }
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={styles.buttons}>
+                    <View style={styles.containerCamera}>
+                        <TouchableOpacity 
+                            style={styles.touchableCamera}>
+                            <View style={styles.containertextCamera}>
+                                
+                                <Text style={styles.textCamera}>Camera</Text>
+                            </View>
+                            
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.containerGalley}>
+                        <TouchableOpacity 
+                            style={styles.touchableGallery}>
+                            <View style={styles.containertextGallery}>
+                                
+                                <Text style={styles.textGallery}>Gallery</Text>
+                            </View>
+                            
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.containerImage}>
+                    <Image 
+                        style={styles.image}
+                        source={this.state.image !== "" ? this.state.image : require('../assets/noimage.jpeg')}/>
+                </View>
+            </SafeAreaView>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        flex : 1 , 
+        flexDirection : 'column'
+    },
+    buttons:{
+        paddingHorizontal : 20 ,
+        flex : 0.15, 
+        flexDirection : 'row' , 
+        justifyContent : 'space-between' , 
+        alignItems : 'center'
+    },
+    containerCamera:{ 
+        alignItems : 'flex-start' 
+    },
+    touchableCamera:{
+        margin : 5 ,
+        padding : 15 , 
+        borderRadius : 5 , 
+        borderWidth : 1, 
+        backgroundColor : '#DEE0E6'
+    },
+    containertextCamera:{
+        flexDirection : 'row', 
+        justifyContent : 'space-around'
+    },
+    textCamera:{
+        marginLeft : 10 ,
+        color : 'red' , 
+        fontSize : 17
+    },
+    containerGalley:{ 
+        alignItems : 'flex-start' 
+    },
+    touchableGallery:{
+        margin : 5 , 
+        padding : 15 , 
+        borderRadius : 5 , 
+        borderWidth : 1, 
+        backgroundColor : '#DEE0E6'
+    },
+    containertextGallery:{
+        flexDirection : 'row'
+    },
+    textGallery:{
+        marginLeft : 10 , 
+        color : 'red' , 
+        fontSize : 17
+    },
+    containerImage:{
+        flex : 0.85 , 
+        justifyContent : 'center' , 
+        alignItems : 'center'
+    },
+    image:{
+        width : 300 , 
+        height : 300
+    }
+
+})
