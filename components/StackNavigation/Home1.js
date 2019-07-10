9201850a1b4d5a80328cb6565123b4f89157755b
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-07-10 18:18:30 +0700

    a

--- components/StackNavigation/Home1.js
+++ components/StackNavigation/Home1.js
@@ -2,6 +2,16 @@ import React, { PureComponent } from 'react'
 import { Text, View } from 'react-native'
 
 export default class Home1 extends PureComponent {
+    static navigationOptions = {
+        title: 'Home',
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+    };
     render() {
         return (
             <View>
